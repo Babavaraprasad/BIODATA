300af380f6ea2c2c2b3937811850476c2c5ae9f9
Babavaraprasad <bapi@itu.dk>
2023-01-31 00:00:44 +0100

    added contact details page - used flaticons as images of gmail,liinkedin,github -styling changes

--- biodata/src/Pages/Contact.js
+++ biodata/src/Pages/Contact.js
@@ -1,1 +1,22 @@
-export const Contact=()=>{return(<h2>This is Contact page</h2>);}
+import "./Contact.css";
+import Gmail from "../Images/Gmail.png";
+import LinkedIn from "../Images/LinkedIn.png";
+import Github from "../Images/Github.png";
+import { useNavigate } from "react-router";
+
+export const Contact=()=>{
+    const navigate=useNavigate();
+    const gmail="https://gmail.google.com/inbox/";
+    function navigatetoGmail(){
+        navigate(gmail);}
+    return(
+    
+<div className="conatct-info">
+<h2>Contact Info</h2>
+   <h4><p>Mobile : +45 28157743</p></h4>
+   <h4><p>Address : Egedalsvaenge 33, 1 mf, 2980, kokkedal, Denmark</p></h4>  
+   <img src={Gmail} onClick={navigatetoGmail}></img>
+   <img src={LinkedIn}></img>
+   <img src={Github}></img>
+</div>
+);}
